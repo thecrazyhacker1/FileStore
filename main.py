a652fc3c785c37c065de18de9d1aeaf594d4530e
thecrazyhacker1 <thecrazyhacker.rraj@gmail.com>
2025-05-07 23:50:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-from pyrogram import utils
+from bot import Bot
+import pyrogram.utils
 
-def get_peer_type_new(peer_id: int) -> str:
-    peer_id_str = str(peer_id)
-    if not peer_id_str.startswith("-"):
-        return "user"
-    elif peer_id_str.startswith("-100"):
-        return "channel"
-    else:
-        return "chat"
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
-utils.get_peer_type = get_peer_type_new
+if __name__ == "__main__":
+    Bot().run()
